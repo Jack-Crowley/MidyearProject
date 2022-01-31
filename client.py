6dca323b80bbec6de16e852db03991108c64c2c4
TKDPenguin <98178409+TKDPenguin@users.noreply.github.com>
2022-01-31 12:06:23 -0500

    Added first server and client code

---
+++ client.py
@@ -0,0 +1,53 @@
+import socket
+import errno
+
+HEADER = 16
+PORT = 8080
+IP = socket.gethostbyname('LAPTOP-I3NGMCO0')
+ADDR = (IP, PORT)
+FORMAT = 'utf-8'
+
+my_username = input("Username: ")
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(ADDR)
+client_socket.setblocking(False)
+
+username = my_username.encode(FORMAT)
+username_header = f"{len(username):<{HEADER}}".encode(FORMAT)
+client_socket.send(username_header)
+client_socket.send(username)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client_socket.send(send_length)
+    client_socket.send(message)
+
+while True:
+    message = input(f'{my_username} > ')
+    send(message)
+
+    try:
+        while True:
+            username_header = client_socket.recv(HEADER)
+            if not len(username_header):
+                print('Connection closed by the server')
+                exit()
+            username_length = int(username_header.decode(FORMAT).strip())
+            username = client_socket.recv(username_length).decode(FORMAT)
+            message_header = client_socket.recv(HEADER)
+            message_length = int(message_header.decode(FORMAT).strip())
+            message = client_socket.recv(message_length).decode(FORMAT)
+
+            print(f'{username} > {message}')
+    except IOError as e:
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
+            print('Reading error: {}'.format(str(e)))
+            exit()
+    except Exception as e:
+        # Any other exception - something happened, exit
+        print('Reading error: '.format(str(e)))
+        exit()
---
+++ server.py
@@ -0,0 +1,60 @@
+import socket
+import threading
+
+HEADER = 16
+PORT = 8080
+IP = socket.gethostbyname(socket.gethostname())
+ADDR = (IP, PORT)
+FORMAT = 'utf-8'
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(ADDR)
+
+sockets_list = [server_socket]
+clients = {}
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+
+    try:
+        msg_len = conn.recv(HEADER)
+        message_length = msg_len.decode(FORMAT)
+        if not len(msg_len):
+            return False
+        message_length = int(message_length)
+        user = {'header': msg_len, 'data': conn.recv(message_length)}
+    except:
+        pass
+    
+    clients[conn] = user
+    sockets_list.append(conn)
+
+    connected = True
+    while connected:
+        msg_len = conn.recv(HEADER).decode(FORMAT)
+        if msg_len: 
+            msg_len = int(msg_len)
+            msg = conn.recv(msg_len).decode(FORMAT)
+
+            user = clients[conn]
+            for client_socket in clients:
+                if client_socket != conn:
+                    client_socket.send(user['header'] + user['data'] + f'{msg_len:<{HEADER}}'.encode(FORMAT) + msg.encode(FORMAT))
+                    print(msg)
+        else:
+            connected = False
+    
+    conn.close()
+
+def start():
+    server_socket.listen(999)
+    print(f"[LISTENING] Server is listening on {IP}")
+    while True:
+        conn, addr = server_socket.accept()
+        print('Accepted new connection from {}:{}'.format(*addr))
+        thread = threading.Thread(target = handle_client, args = (conn, addr))
+        thread.start()
+        print(f'[ACTIVE CONNECTIONS] {threading.active_count() -1}')
+
+print("[STARTING] server is starting...")
+start()
