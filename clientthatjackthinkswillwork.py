c7cc998130a656f07853d44ceb9cbc29d0bbfc21
Jacob Berger <tkdpenguin123@gmail.com>
2022-02-02 12:34:36 -0500

    Changed IP addr

--- clientthatjackthinkswillwork.py
+++ clientthatjackthinkswillwork.py
@@ -4,7 +4,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.1.26"
+SERVER = "54.197.15.97"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
