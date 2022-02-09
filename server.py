616404f9839c60a1ba7b76d3e8391ab035e04d5f
Jacob Berger <tkdpenguin123@gmail.com>
2022-02-09 12:36:33 -0500

    hopefully working server

--- server.py
+++ server.py
@@ -19,12 +19,10 @@ def handle_client(conn, addr):
     try:
         msg_len = conn.recv(HEADER)
         message_length = msg_len.decode(FORMAT)
-        if not len(msg_len):
-            return False
         message_length = int(message_length)
         user = {'header': msg_len, 'data': conn.recv(message_length)}
     except:
-        pass
+        print('unable to get username')
     
     sockets_list.append(conn)
     ignoreDisconnected = []
