24ca5b02ff4c78fddf7c455a00f84e8cb83a4e00
Parth Jain <parthjain22@gmail.com>
2022-02-17 16:10:18 -0500

    Added accounts file as database (for now) and created login system

--- loginscreen.py
+++ loginscreen.py
@@ -1,5 +1,14 @@
 import pygame
+import os
+import hashlib
 from shapes import *
+
+with open('accounts.txt') as accs:
+    accounts = {}
+    for line in accs:
+        line = line.split(';')
+        accounts[line[0]] = line[1].strip()
+    accs.close()
 
 class Login:
     def __init__(self,window,clock,pixelratio,validChars):
@@ -51,6 +60,11 @@ class Login:
                             if button.click(mousex, mousey):
                                 self.newScreen = "register"
                                 self.run = False
+                        elif button.command == 'check_login':
+                            if button.click(mousex, mousey):
+                                if self.checkLogin(self.username.textMessage, self.password.textMessage):
+                                    button.command = 'new_screen'
+                                    self.run = False
                 if self.active != None:
                     if event.type == pygame.KEYDOWN:
                         if event.key == pygame.K_BACKSPACE:
@@ -88,6 +102,14 @@ class Login:
                     
             
             self.draw()
+    
+    def checkLogin(self, username, password):
+        if accounts[username]:
+            salt = b'jackbad'
+            plaintext = password.encode()
+            digest = hashlib.pbkdf2_hmac('sha256', plaintext, salt, 10000)
+            return digest.hex() == accounts[username]
+        return False
         
     def loadDrawables(self):
         self.drawables.append(Rectangle(0,0,1920,192,(17,17,17),self.window,self.pixelratio))
@@ -101,7 +123,7 @@ class Login:
         self.createInputField(726,600,468,75,(17,17,17),self.window,self.pixelratio,"input_field",(2,217,198),"password","Enter Password...",(193,146,252),self.validChars,75)
         self.drawables.append(Rectangle(726,675,468,15,(2,217,198),self.window,self.pixelratio))
 
-        self.createButton(726,740,468,75,(193,146,252),self.window,self.pixelratio,command="new_screen")
+        self.createButton(726,740,468,75,(193,146,252),self.window,self.pixelratio,command="check_login")
         self.drawables.append(Text("Orbitron",(255,255,255), "LOGIN",self.window,960,777,self.pixelratio,75))
 
         self.createButton(726,855,468,75,(193,146,252),self.window,self.pixelratio,command="register")
